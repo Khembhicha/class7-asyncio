ffd741f2d530b9b6c5083b40ce9cc1851f584a0c
Khembhicha <6310301032@cdti.ac.th>
2023-08-16 14:33:05 +0700

    commit

--- 1-2-comic-flask/3-async_flask_api_with_httpx.py
+++ 1-2-comic-flask/3-async_flask_api_with_httpx.py
@@ -1,26 +1,29 @@
-# https://flask.palletsprojects.com/en/2.3.x/
-# https://www.python-httpx.org/async/
-# https://flask.palletsprojects.com/en/2.3.x/quickstart/#rendering-templates
-
-import asyncio
 import time
 from random import randint
-import httpx
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# function converted to coroutine
-async def get_xkcd_image(session): # dont wait for the response of API
-    
 
-# function converted to coroutine
-async def get_multiple_images(number): 
-    async with 
+def get_xkcd_image():
+    comcid = randint(0, 1000)
+    response = requests.get(f'https://xkcd.com/{comcid}/info.0.json')
+    return response.json()['img']
 
-@app.get('/comic')
-async def hello(): 
-    
+
+def get_multiple_images(number):
+    return [get_xkcd_image() for _ in range(number)]
+
+
+@app.route('/comic')
+def hello():
+    start = time.perf_counter()
+    # Corrected: Call get_multiple_images() with an argument
+    urls = get_multiple_images(20)
+    end = time.perf_counter()
+    return render_template('index.html', end=end, start=start, urls=urls)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
